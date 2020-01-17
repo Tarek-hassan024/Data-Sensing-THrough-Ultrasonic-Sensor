956c677706af573a94de1ef640040d6c655035d3
Tarek Hassan <hasantareq60@gmail.com>
2020-01-17 17:35:34 +0600

    Add files via upload

---
+++ Sensing_Unit.ino
@@ -0,0 +1,37 @@
+int trig = 14;
+int echo = 12;
+
+float Tt;
+float s = 0;
+float v = 0.0344;
+float t = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    digitalWrite(trig,LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trig,HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(trig,LOW);
+   
+   Tt = pulseIn(echo,HIGH);
+    t = (float)Tt/2;
+
+    s = v*t;
+    Serial.println(s);
+}
---
+++ Ultrasonic_Sensor.ino
@@ -0,0 +1,179 @@
+int trig = 13;
+int echo = 12;
+
+float Tt;
+float s = 0;
+float v = 0.0344;
+float t = 0;
+
+#include <WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// the on off button feed turns this LED on/off
+#define LED 26  
+// the slider feed sets the PWM output of this pin
+#define PWMOUT 12
+
+/************************* WiFi Access Point *********************************/
+#define WLAN_SSID       "RUET--Teachers"
+#define WLAN_PASS       "RUET@2018"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "HTarek024"
+#define AIO_KEY         "aio_uVEz43NQowOjafo5wWsXfrBUJkvH"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+
+/****************************** Subscribe Part for only One ***************************************/
+
+
+//Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/FirstSubscribe");
+
+/****************************** Publish For only One ***************************************/
+
+Adafruit_MQTT_Publish ap = Adafruit_MQTT_Publish(&mqtt,AIO_USERNAME "/feeds/Status");
+
+//Adafruit_MQTT_Subscribe slider = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/slider");
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+float ledstatus ;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  //pinMode(PWMOUT, OUTPUT);
+  Serial.begin(9600);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for onoff & slider feed.
+//  mqtt.subscribe(&onoffbutton);
+  //mqtt.subscribe(&slider);
+}
+
+uint32_t x=0;
+
+
+
+void loop() {
+ 
+  MQTT_connect();
+ ledstatus = DATA();
+ 
+//  Adafruit_MQTT_Subscribe *subscription;
+//  while ((subscription = mqtt.readSubscription(5000))) {
+//
+//    
+//    if (subscription == &onoffbutton) {
+//      Serial.print(F("On-Off button: "));
+//      Serial.println((char *)onoffbutton.lastread);
+//      
+//      if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
+//        digitalWrite(LED, HIGH); 
+//        ledstatus = DATA();
+//        //DATA();
+//      }
+//      if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
+//        digitalWrite(LED, LOW); 
+//        ledstatus = 0;
+//      }
+//--------------------------------------------For Publish Issue-----------------------------------------
+      if (! ap.publish(ledstatus))
+        Serial.println(F("Publish Failed."));
+      else {
+        Serial.println(F("Publish Success!"));
+        delay(500);
+      }  
+
+  // ping the server to keep the mqtt connection alive
+  
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+    delay(100);
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+float DATA()
+{
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trig, LOW);
+
+  Tt = pulseIn(echo, HIGH);
+  t = (float)Tt / 2;
+
+  s = v*t;
+
+}
